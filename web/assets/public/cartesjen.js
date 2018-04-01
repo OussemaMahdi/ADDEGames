7c5bf31ff734397b694cde03684bdf9f4ff7a2da
Oussema Mahdi <oussema.mahdi@live.fr>
2018-04-01 18:36:19 +0200

    update

--- web/assets/public/cartesjen.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$( "#carte_tangible" ).click(function() {
-		alert( "Cette fonctionnalité n'est pas encore disponible" );
-	});
-
-	$( "#carte_numerique").click(function() {
-		$('div#container_cartesjen').toggleClass( "hidden");
-	});
-});
